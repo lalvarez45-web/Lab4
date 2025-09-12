dab2a31fdb0d04b8238c78604842a70873752fce
lalvarez45-web <lalvarez45@horizon.csueastbay.edu>
2025-09-12 11:07:40 -0700

    Fixed Menu

--- MathSuite.cpp
+++ MathSuite.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 #include <iomanip>
-#include <string>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 using namespace std;
 
 int main() {
-    string playerName;
-    cout << "Welcome to !\n";
-    cout << "Enter your full name: ";
-    getline(cin, playerName);
+    srand(time(0)); // seed random numbers
 
-    int score = 0;
-    int rounds = 0;
-    bool quit = false;
+    int choice;
+    int correct = 0, wrong = 0;
+    int a, b, answer, userAnswer;
 
-    int &outerScoreRef = score;
+    ofstream outFile("MathPracticeResults.txt", ios::app);
 
-    srand(time(0));
+    cout << "===== Math Practice Suite =====" << endl;
 
-    while (!quit) {
-       
-        cout << "\n=== Console Arcade Menu ===\n";
-        cout << "1. Dice Duel\n";
-        cout << "2. Pattern Printer\n";
-        cout << "3. Show Scoreboard\n";
-        cout << "4. Quit\n";
-        cout << "Choose an option: ";
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Addition\n";
+        cout << "2. Subtraction\n";
+        cout << "3. Multiplication\n";
+        cout << "4. Division\n";
+        cout << "5. Statistics\n";
+        cout << "-999 to Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-        
+        if (choice == -999) {
+            cout << "\nExiting Math Practice Suite...\n";
+            break;
+        }
     }
 }
