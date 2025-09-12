96f3d93bc9b83b7a0a63962f7431f608c4f49e5b
lalvarez45-web <lalvarez45@horizon.csueastbay.edu>
2025-09-12 11:12:24 -0700

    Changed

--- MathSuite.cpp
+++ MathSuite.cpp
@@ -6,30 +6,25 @@
 using namespace std;
 
 int main() {
-    srand(time(0)); // seed random numbers
+    srand(time(0));
 
-    int choice;
     int correct = 0, wrong = 0;
-    int a, b, answer, userAnswer;
+    char choice;
+    bool running = true;
 
-    ofstream outFile("MathPracticeResults.txt", ios::app);
+    ofstream outFile("results.txt", ios::app); // Save results
 
-    cout << "===== Math Practice Suite =====" << endl;
+    cout << "===== Math Practice App =====" << endl;
 
-    do {
+    while (running) {
         cout << "\nMenu:\n";
-        cout << "1. Addition\n";
-        cout << "2. Subtraction\n";
-        cout << "3. Multiplication\n";
-        cout << "4. Division\n";
-        cout << "5. Statistics\n";
-        cout << "-999 to Exit\n";
+        cout << "1) Addition\n";
+        cout << "2) Subtraction\n";
+        cout << "3) Multiplication\n";
+        cout << "4) Division\n";
+        cout << "5) Statistics\n";
+        cout << "Q) Quit\n";
         cout << "Enter your choice: ";
         cin >> choice;
-
-        if (choice == -999) {
-            cout << "\nExiting Math Practice Suite...\n";
-            break;
-        }
     }
 }
